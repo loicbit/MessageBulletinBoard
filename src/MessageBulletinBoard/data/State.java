ed92cdaf7176d7d8fb45dd501cfd4ca16168b0de
Loïc Barbe <loic.barbe1@gmail.com>
2021-09-30 12:43:18 +0200

    added state object

---
+++ src/MessageBulletinBoard/data/State.java
@@ -0,0 +1,9 @@
+package MessageBulletinBoard.data;
+
+import java.io.Serializable;
+
+public class State implements Serializable {
+    private String publicKey = null;
+    private int cellLocationIndex = -1;
+    private String tag = null;
+}
---
+++ src/MessageBulletinBoard/data/User.java
@@ -0,0 +1,4 @@
+package MessageBulletinBoard.data;
+
+public class User {
+}
